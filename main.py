7cd33e5787b056ec81a5e437c1419452296a33e6
shivsingh2504 <shivsinghs2504@gmail.com>
2026-03-17 01:26:00 +0530

    Creating a POI Density graph where Denisty is Count/Area in main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.data.data_load import load_city
 from src.data.poi_fetch import fetch_pois
 import geopandas as gpd
+import matplotlib.pyplot as plt
 city = load_city("data/raw/ZillowNeighborhoods-NY.shp")
+city = city[city["City"] == "New York"]
 print(city.head())
 print(city.columns)
-pois = fetch_pois("Manhattan, New York, USA")
+pois = fetch_pois("New York, USA")
 print("POIs fetched!")
 print(pois.head())
 
@@ -14,9 +16,24 @@ joined = gpd.sjoin(pois,city,predicate="within")
 print("Spatial join complete!!")
 print(joined.head())
 
-poi_counts = joined.groupby("Name").size().reset_index(name="poi_count")
+poi_counts = joined.groupby("Name").size().reset_index(name="POI Count")
 print(poi_counts.head())
 
 city_with_counts = city.merge(poi_counts, on="Name", how="left")
-city_with_counts["poi_count"] = city_with_counts["poi_count"].fillna(0)
-print(city_with_counts[["Name","poi_count"]]).head()
+city_with_counts["POI Count"] = city_with_counts["POI Count"].fillna(0)
+print(city_with_counts[["Name","POI Count"]].head())
+city_with_counts["Area"] = city_with_counts.geometry.area
+city_with_counts["POI Density"]=(city_with_counts["POI Count"]/city_with_counts["Area"])
+print(city_with_counts[["Name","POI Count","POI Density"]].head())
+city_with_counts["Dead Zone"] = city_with_counts["POI Count"]<10
+print(city_with_counts[["Name","POI Count","Dead Zone"]].head())
+
+city_with_counts.plot(
+  column="POI Density",
+  cmap="Reds",
+  legend=True,
+  edgecolor="black",
+  linewidth=0.3,
+  figsize=(12,10)
+)
+plt.show()
