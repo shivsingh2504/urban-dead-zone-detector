f9c5dd7f2f0fe7aafd15509a493eb6ac5206c26c
shivsingh2504 <shivsinghs2504@gmail.com>
2026-03-17 00:20:57 +0530

    Merging POI Counts in main.py

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ print(joined.head())
 
 poi_counts = joined.groupby("Name").size().reset_index(name="poi_count")
 print(poi_counts.head())
+
+city_with_counts = city.merge(poi_counts, on="Name", how="left")
+city_with_counts["poi_count"] = city_with_counts["poi_count"].fillna(0)
+print(city_with_counts[["Name","poi_count"]]).head()
