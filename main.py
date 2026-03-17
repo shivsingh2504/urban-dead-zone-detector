b745fb17cd76b2f5bec37c143914c63acad89cec
shivsingh2504 <shivsinghs2504@gmail.com>
2026-03-18 01:09:23 +0530

    Added Zone Classification in main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.data.data_load import load_city
 from src.data.poi_fetch import fetch_pois
-from src.features.dead_zone import detect_dead_zones
 from src.utils.geo_utils import perform_spatial_join
 from src.features.density import calculate_density
+from src.analysis.detect_zones import classify_zones
 from src.visual.map_visual import plot_density, plot_dead_zones
+
 
 def main():
     # Load data
@@ -22,15 +23,20 @@ def main():
     city = calculate_density(city, joined)
     print("Density Calculated")
 
-    # Dead Zone Detection
-    city = detect_dead_zones(city)
-    print("Dead Zones Identified")
+    # Zone Classification (NEW LOGIC)
+    city = classify_zones(city)
+    print("Zones Classified")
 
-    print(city[["Name", "POI Count", "POI Density", "Dead Zone"]].head())
-    
-    dead_zones = city[city["Dead Zone"]==True]
+    # Preview
+    print(city[["Name", "POI Density", "Zone Type"]].head())
+
+    # Get worst dead zones
+    dead_zones = city[city["Zone Type"] == "Dead Zone"]
     dead_zones_sorted = dead_zones.sort_values(by="POI Density")
-    print(dead_zones_sorted[["Name","POI Density"]].head(10))
+
+    print("\nTop 10 Worst Dead Zones:")
+    print(dead_zones_sorted[["Name", "POI Density"]].head(10))
+
     # Visualization
     plot_density(city)
     plot_dead_zones(city)
